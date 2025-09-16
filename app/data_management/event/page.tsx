c07ab9c46f14bfde872add943766979b73b87e64
Coen <c.horrevoets@gmail.com>
2025-09-16 14:21:45 +1000

    copy(event-drawer): rename 'Properties' tab to 'Event Details'

--- app/data_management/event/page.tsx
+++ app/data_management/event/page.tsx
@@ -393,7 +393,7 @@ export default function EventsPage() {
                                                 <TabsList>
                                                     <TabsTrigger value="properties" className="text-lg data-[state=active]:bg-white data-[state=active]:text-black">
                                                         <span className="flex items-center gap-2">
-                                                            Properties
+                                                            Event Details
                                                             <span className="inline-flex items-center rounded-full bg-red-100 px-2.5 py-0.5 text-xs font-medium text-red-800 ring-1 ring-inset ring-red-200">5 Errors</span>
                                                         </span>
                                                     </TabsTrigger>
